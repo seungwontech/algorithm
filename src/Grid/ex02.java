a2b2b4c5e697c8e1a568016d1b92864681451cb2
sw.lee <swlee173@gmail.com>
2021-08-24 23:41:15 +0900

    그리드 알고리즘(1이 될때까지)

--- src/Grid/ex02.java
+++ src/Grid/ex02.java
@@ -1,25 +1,23 @@
 package Grid;
 
 public class ex02 {
-    public static void main(String[] age){
-
+    public static void main(String[] age) {
         int n = 25;
         int k = 3;
         int result = 0;
         int target;
 
-        while(true){
+        while (true) {
 
-            target = (n/k)*k;           // 25/3*3 =24   , 8/3*3= 6,  2/3*2= 0
+            target = (n / k) * k;       // 25/3*3 =24   , 8/3*3= 6,  2/3*2= 0
             result += n - target;       // 25-24+0=1    , 8-6+2 =4,  2-0+5=7
             n = target;                 // n = 24       , n = 6,     n=0
 
-            if(n < k){                  //2<3
+            if (n < k) {                //2<3
                 break;
             }
-
-            result = result +1;         //1+1 = 2, 4+1=5
-            n=n/k;                      //24/3 =8, 6/3 =2
+            result = result + 1;        //1+1 = 2, 4+1=5
+            n = n / k;                  //24/3 =8, 6/3 =2
         }
         result = result + (n - 1);      // 7+(0-1) = 6
         System.out.println(result);
