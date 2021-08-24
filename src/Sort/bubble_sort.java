15a4ba3648433e6baf76363e9ceafaa4e153cf96
sw.lee <swlee173@gmail.com>
2021-08-24 23:31:24 +0900

    버블 정렬 알고리즘

--- src/Sort/bubble_sort.java
+++ src/Sort/bubble_sort.java
@@ -1,4 +1,21 @@
 package Sort;
 
 public class bubble_sort {
+    public static void main(String[] ages) {
+        int i, j, temp;
+        int[] array = {1, 10, 3, 4, 7, 5, 6};
+
+        for (i = 0; i < array.length; i++) {
+            for (j = 0; j < array.length - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        for (i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
 }
