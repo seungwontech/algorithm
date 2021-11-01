d1f8b74b9a5300e2382d36965dbc9aa52323cacf
sw.lee <swlee173@gmail.com>
2021-11-01 23:42:37 +0900

    백준 2750번 문제 - 수 정렬하기

---
+++ src/BaekJoon/ex2750.java
@@ -0,0 +1,31 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+// 단계별로 풀어보기 - 정렬
+// 문제 번호 2750 - 수 정렬하기
+public class ex2750 {
+    public static void main(String[] age) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextInt();
+        }
+        int temp;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
