9cfbaae887b890e787ae6b8d51d34f9015efc280
sw.lee <swlee173@gmail.com>
2021-11-03 00:04:54 +0900

    백준 1427번 문제 - 소트인사이드

--- src/BaekJoon/ex1427.java
+++ src/BaekJoon/ex1427.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 // 단계별로 풀어보기 - 정렬
-// 문제 번호 1427 - 소트인사이드
+// 백준 1427번 문제 - 소트인사이드
 public class ex1427 {
     public static void main(String[] args) throws IOException {
 
