d4b0176d4cbd0286bcd02b345d1a8441f62845d4
sw.lee <swlee173@gmail.com>
2022-06-05 23:30:21 +0900

    힙 정렬 알고리즘

---
+++ src/Sort/heap_sort.java
@@ -0,0 +1,47 @@
+package Sort;
+
+public class heap_sort {
+
+    static void swap(int[] a, int idx1, int idx2) {
+        int temp = a[idx1];
+        a[idx1] = a[idx2];
+        a[idx2] = temp;
+    }
+
+    static void downHeap(int[] a, int left, int right) {
+        int temp = a[left];
+        int child, parent;
+
+        for (parent = left; parent < (right + 1) / 2; parent = child) {
+            int cl = parent * 2 + 1;
+            int cr = cl + 1;
+            child = (cr <= right && a[cr] > a[cl]) ? cr : cl;
+
+            if (temp >= a[child])
+                break;
+
+            a[parent] = a[child];
+        }
+        a[parent] = temp;
+    }
+
+    static void heapSort(int[] a, int n) {
+        for (int i = (n - 1) / 2; i >= 0; i--)
+            downHeap(a, i, n - 1);
+
+        for (int i = n - 1; i > 0; i--) {
+            swap(a, 0, i);
+            downHeap(a, 0, i - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 4, 5, 7, 9, 8, 35, 2, 3, 11, 20};
+
+        heapSort(a, a.length);
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
