5c3a29f85ad4677f337ca5c60f4fad31b9473b19
sw.lee <swlee173@gmail.com>
2022-10-14 16:46:06 +0900

    기사문제테스트

--- src/Test/test_04.java
+++ src/Test/test_04.java
@@ -1,23 +1,25 @@
 package Test;
 
-
-
-class P{
-    public void f1(){
+class P {
+    public void f1() {
         System.out.println("A");
     }
-    public void f2(){
+
+    public void f2() {
         System.out.println("B");
     }
 }
-class C extends P{
-    public void f1(){
+
+class C extends P {
+    public void f1() {
         System.out.println("C");
     }
-    public void f3(){
+
+    public void f3() {
         System.out.println("D");
     }
 }
+
 public class test_04 {
     public static void main(String[] args) {
         C a = new C();
