4bce8dc372c419fadfbaa54df54a0499ae6cc340
sw.lee <swlee173@gmail.com>
2022-10-14 16:45:08 +0900

    기사문제테스트

---
+++ src/Test/test_04.java
@@ -0,0 +1,28 @@
+package Test;
+
+
+
+class P{
+    public void f1(){
+        System.out.println("A");
+    }
+    public void f2(){
+        System.out.println("B");
+    }
+}
+class C extends P{
+    public void f1(){
+        System.out.println("C");
+    }
+    public void f3(){
+        System.out.println("D");
+    }
+}
+public class test_04 {
+    public static void main(String[] args) {
+        C a = new C();
+        a.f1();
+        a.f2();
+        a.f3();
+    }
+}
