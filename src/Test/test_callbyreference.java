0ee22a23de2954051d88cf7ef9e765515b66da0c
sw.lee <swlee173@gmail.com>
2023-01-15 17:24:34 +0900

    블로그_call by reference

---
+++ src/Test/test_callbyreference.java
@@ -0,0 +1,43 @@
+package Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class test_callbyreference {
+    public static void main(String[] args) {
+
+        Map<String, Object> info = new HashMap<String, Object>();
+        info.put("이름", "짱구");
+        info.put("국적", "일본");
+
+        System.out.println(info);
+        // {국적=일본, 이름=짱구}
+
+        addInfo(info);
+        System.out.println(info);
+        // {국적=일본, 이름=신짱구, 나이=7살}
+
+        removeInfo(info);
+        System.out.println(info);
+        // 결과: {이름=신짱구, 나이=7살}
+    }
+
+    /**
+     * 추가
+     * @param info
+     */
+    public static void addInfo(Map<String, Object> info) {
+        Map<String, Object> rtnMap = info;
+        rtnMap.put("나이", "7살");
+        rtnMap.put("이름", "신짱구");
+    }
+
+    /**
+     * 삭제
+     * @param info
+     */
+    public static void removeInfo(Map<String, Object> info) {
+        Map<String, Object> rtnMap = info;
+        rtnMap.remove("국적");
+    }
+}
