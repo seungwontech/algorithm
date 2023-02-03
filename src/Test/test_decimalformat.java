42b20ef0aa242574c288537ecc4560fb632c08ed
sw.lee <swlee173@gmail.com>
2023-02-03 13:29:30 +0900

    decimalformat 테스트

---
+++ src/Test/test_decimalformat.java
@@ -0,0 +1,11 @@
+package Test;
+
+import java.text.DecimalFormat;
+
+public class test_decimalformat {
+    public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("###,###,###");
+        String bbr = "90000000000";
+        System.out.println(df.format(Long.parseLong(bbr)));
+    }
+}
