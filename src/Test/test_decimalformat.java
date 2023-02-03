d32d5519e5fd174b43af1b76a7fdc61a2dc9323d
sw.lee <swlee173@gmail.com>
2023-02-03 13:33:51 +0900

    decimalformat 테스트

--- src/Test/test_decimalformat.java
+++ src/Test/test_decimalformat.java
@@ -6,6 +6,7 @@ public class test_decimalformat {
     public static void main(String[] args) {
         DecimalFormat df = new DecimalFormat("###,###,###");
         String bbr = "90000000000";
-        System.out.println(df.format(Long.parseLong(bbr)));
+        System.out.println(df.format(Long.parseLong(bbr))); // 90,000,000,000
+
     }
 }
