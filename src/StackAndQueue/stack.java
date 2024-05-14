05abb8e6d375fe5ce7c4a77575e6075829b7e2ee
sw.lee <swlee173@gmail.com>
2024-05-14 15:14:25 +0900

    스택

--- src/StackAndQueue/stack.java
+++ src/StackAndQueue/stack.java
@@ -11,13 +11,13 @@ public class stack {
         stack.push(2);
         stack.push(4);
 
-        System.out.println(stack.contains(2)); // 2라는 숫자 존재여부 판단 있으면 true
-
-        System.out.println(stack.peek());      // 스택의 가장 상단의 값 출력
-
-        stack.pop();                           // 값 제거
+        System.out.println("contains : " + stack.contains(2)); // 2라는 숫자 존재여부 판단 있으면 true
         System.out.println(stack);
+        System.out.println("peek : " + stack.peek());          // 스택의 가장 상단의 값 출력하고 제거하지 않음
+        System.out.println(stack);
+        System.out.println("pop : " + stack.pop());            // 스택의 가장 상단의 값 출력하고 제거함
 
+        System.out.println(stack);
         stack.clear();                         // 초기화
         System.out.println(stack);
         System.out.println(stack.isEmpty());   // 비어있는지 판단 비어있으면 true
