606ff0b259558891a4325c77287afb8ec431ffe1
sw.lee <swlee173@gmail.com>
2024-05-14 11:15:20 +0900

    11047번 동전 0

---
+++ src/BaekJoon/ex11047.java
@@ -0,0 +1,33 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class ex11047 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader brf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(brf.readLine());
+        int[] aArr = new int[2];
+        for (int i = 0; i < aArr.length; i++) {
+            aArr[i] = Integer.parseInt(st.nextToken());
+        }
+        Integer[] bArr = new Integer[aArr[0]];
+        for (int i = 0; i < aArr[0]; i++) {
+            bArr[i] = Integer.parseInt(brf.readLine());
+        }
+        Arrays.sort(bArr, Collections.reverseOrder());
+        int money = aArr[1];
+        int count = 0;
+        for (int value : bArr) {
+            count += money / value;
+            if (count > 0) {
+                money = money % value;
+            }
+        }
+        System.out.println(count);
+    }
+}
