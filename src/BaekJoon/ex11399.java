ede4004395df4ca06414abaf1e7c5751431c75e6
sw.lee <swlee173@gmail.com>
2024-05-14 11:15:48 +0900

    11399번 ATM

---
+++ src/BaekJoon/ex11399.java
@@ -0,0 +1,27 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class ex11399 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader brf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(brf.readLine());
+        int[] timeArr = new int[n];
+        StringTokenizer st = new StringTokenizer(brf.readLine());
+        for (int i = 0; i < n; i++) {
+            timeArr[i] = Integer.parseInt(st.nextToken());
+        }
+        int answer = 0;
+        int answerSum = 0;
+        Arrays.sort(timeArr);
+        for(int i = 0; i < n; i++) {
+            answer += timeArr[i];
+            answerSum += answer;
+        }
+        System.out.println(answerSum);
+    }
+}
