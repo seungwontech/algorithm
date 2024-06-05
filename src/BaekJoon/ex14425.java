6caa8eec7c10599f84fc0ac4251d71b7a26e733d
seungwon <swlee173@gmail.com>
2024-06-05 17:22:23 +0900

    14425번 문자열 집합

---
+++ src/BaekJoon/ex14425.java
@@ -0,0 +1,27 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class ex14425 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Map<String, Object> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(br.readLine(), 0);
+        }
+        int cnt = 0;
+        for (int i = 0; i < m; i++) {
+            if (map.containsKey(br.readLine())) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
