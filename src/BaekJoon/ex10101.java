320129006ea1f52b6f1ef882efb7b8a4db017e35
seungwon <swlee173@gmail.com>
2024-06-15 00:08:34 +0900

    10101 삼각형 외우기

---
+++ src/BaekJoon/ex10101.java
@@ -0,0 +1,28 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ex10101 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+
+        String answer = "";
+        if (a + b + c != 180) {
+            answer = "Error";
+        } else {
+            if (a == 60 && b == 60 && c == 60) {
+                answer = "Equilateral";
+            } else if( a == b || b == c || a == c){
+                answer = "Isosceles";
+            } else {
+                answer = "Scalene";
+            }
+        }
+        System.out.println(answer);
+    }
+}
