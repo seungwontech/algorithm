12ec04ef1c1d7cbe06fb34b815e5209548d6ac34
seungwon <swlee173@gmail.com>
2024-06-16 15:56:01 +0900

    23813 회전

---
+++ src/BaekJoon/ex23813.java
@@ -0,0 +1,18 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ex23813 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int rept = Integer.parseInt("1".repeat(str.length()));
+        long sum = 0;
+        for (char ch : str.toCharArray()) {
+            sum += ((ch - '0') * rept);
+        }
+        System.out.println(sum);
+    }
+}
