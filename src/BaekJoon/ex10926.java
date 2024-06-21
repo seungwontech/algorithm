10e6216b32b1590307e02ad2a1ed0042c6b5de1d
seungwon <swlee173@gmail.com>
2024-06-21 22:13:27 +0900

    10926 ??!

---
+++ src/BaekJoon/ex10926.java
@@ -0,0 +1,12 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ex10926 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(str + "??!");
+    }
+}
