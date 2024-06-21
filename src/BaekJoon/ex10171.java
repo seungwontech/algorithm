0c99d5d88f62ceb09a26ac48016e493b977ef5c8
seungwon <swlee173@gmail.com>
2024-06-21 23:39:45 +0900

    10171 고양이

---
+++ src/BaekJoon/ex10171.java
@@ -0,0 +1,10 @@
+package BaekJoon;
+
+public class ex10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
