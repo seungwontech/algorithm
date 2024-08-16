fbe01a888ddd3c81e20cb6cab4fc864291ba057e
seungwon <swlee173@gmail.com>
2024-08-16 18:40:08 +0900

    브론즈5 - A*B, 사칙연산, A/B, 시험성적

---
+++ src/BaekJoon/Day2/ex1008.java
@@ -0,0 +1,12 @@
+package BaekJoon.Day2;
+
+import java.util.Scanner;
+
+public class ex1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(A/B);
+    }
+}
