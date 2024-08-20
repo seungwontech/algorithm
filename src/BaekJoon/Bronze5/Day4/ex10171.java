77de16bf31537adf7df0ad40c77065437ab36101
seungwon <swlee173@gmail.com>
2024-08-20 14:24:26 +0900

    브론즈5 - 고양이, 개

---
+++ src/BaekJoon/Bronze5/Day4/ex10171.java
@@ -0,0 +1,10 @@
+package BaekJoon.Bronze5.Day4;
+
+public class ex10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}
