a20f15f2211c3432e51cb4bcd5c270380f65684a
seungwon <swlee173@gmail.com>
2024-08-20 15:45:17 +0900

    브론즈5 - A+B - 5, A+B - 7

---
+++ src/BaekJoon/Bronze5/Day8/ex10952.java
@@ -0,0 +1,19 @@
+package BaekJoon.Bronze5.Day8;
+
+import java.util.Scanner;
+
+public class ex10952 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if (a == 0 && b == 0) {
+                break;
+            }
+            System.out.println(a + b);
+
+        }
+    }
+}
---
+++ src/BaekJoon/Bronze5/Day8/ex11021.java
@@ -0,0 +1,15 @@
+package BaekJoon.Bronze5.Day8;
+
+import java.util.Scanner;
+
+public class ex11021 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 1; i <= t; i++) {
+            int a = sc.nextInt();
+            int b= sc.nextInt();
+            System.out.println("Case #" + i + ": " + (a + b));
+        }
+    }
+}
