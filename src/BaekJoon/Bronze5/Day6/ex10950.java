91308830789e9a41c701b7154e5ffd4bf69d0dc0
seungwon <swlee173@gmail.com>
2024-08-20 14:47:49 +0900

    브론즈5 - A+B -3, N찍기

---
+++ src/BaekJoon/Bronze5/Day6/ex10950.java
@@ -0,0 +1,15 @@
+package BaekJoon.Bronze5.Day6;
+
+import java.util.Scanner;
+
+public class ex10950 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a + b);
+        }
+    }
+}
