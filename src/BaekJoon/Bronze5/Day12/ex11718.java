16b9804940e7b7c0a510bdc7767ae3fb1bd3026f
seungwon <swlee173@gmail.com>
2024-08-21 17:11:01 +0900

    브론즈5 - 등록, 그대로 출력하기

---
+++ src/BaekJoon/Bronze5/Day12/ex11718.java
@@ -0,0 +1,13 @@
+package BaekJoon.Bronze5.Day12;
+
+import java.util.Scanner;
+
+public class ex11718 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){
+            String text = sc.nextLine();
+            System.out.println(text);
+        }
+    }
+}
---
+++ src/BaekJoon/Bronze5/Day12/ex7287.java
@@ -0,0 +1,8 @@
+package BaekJoon.Bronze5.Day12;
+
+public class ex7287 {
+    public static void main(String[] args) {
+        System.out.println(100);
+        System.out.println("sw_lee");
+    }
+}
