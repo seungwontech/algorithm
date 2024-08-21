48411da91befb635bf7542d543ece8f3622c2a8c
seungwon <swlee173@gmail.com>
2024-08-21 16:53:16 +0900

    브론즈5 - 1998년생인 내가 태국에서는 2541년생?!, 팩토리얼, A+B -2

---
+++ src/BaekJoon/Bronze5/Day11/ex18108.java
@@ -0,0 +1,11 @@
+package BaekJoon.Bronze5.Day11;
+
+import java.util.Scanner;
+
+public class ex18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        System.out.println(y - 543);
+    }
+}
