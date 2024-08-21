bc8d5ba68873144a3fd6c18e07bf0005a9b300dd
seungwon <swlee173@gmail.com>
2024-08-21 16:39:41 +0900

    브론즈5 - ??!

---
+++ src/BaekJoon/Bronze5/Day10/ex10926.java
@@ -0,0 +1,11 @@
+package BaekJoon.Bronze5.Day10;
+
+import java.util.Scanner;
+
+public class ex10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.next();
+        System.out.println(text + "??!");
+    }
+}
