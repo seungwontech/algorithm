39a757554ee4802262f2d469fc5a70756bbc0025
seungwon <swlee173@gmail.com>
2024-08-22 17:20:49 +0900

    브론즈5 - 꼬마 정민, 단어 길이 재기, 검증수

---
+++ src/BaekJoon/Bronze5/Day15/ex2743.java
@@ -0,0 +1,11 @@
+package BaekJoon.Bronze5.Day15;
+
+import java.util.Scanner;
+
+public class ex2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String result = sc.next();
+        System.out.println(result.length());
+    }
+}
