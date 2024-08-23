c5e5fcf051f438bbba1346911438baec1ecfc0a4
seungwon <swlee173@gmail.com>
2024-08-23 12:08:07 +0900

    브론즈5 - 코딩은 체육과목 입니다, 문자열

---
+++ src/BaekJoon/Bronze5/Day17/ex9086.java
@@ -0,0 +1,14 @@
+package BaekJoon.Bronze5.Day17;
+
+import java.util.Scanner;
+
+public class ex9086 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        for(int i =0; i < t; i++){
+            String text = sc.nextLine();
+            System.out.println(text.charAt(0) + text.substring(text.length()-1));
+        }
+    }
+}
