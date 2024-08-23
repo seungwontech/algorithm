7d274eac8a536a0bf104dcb430140ca590a01bdc
seungwon <swlee173@gmail.com>
2024-08-23 11:49:53 +0900

    브론즈5 - 과제 안 내신분...?, 큰수 A+B

---
+++ src/BaekJoon/Bronze5/Day16/ex10757.java
@@ -0,0 +1,13 @@
+package BaekJoon.Bronze5.Day16;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class ex10757 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        System.out.println(a.add(b));
+    }
+}
