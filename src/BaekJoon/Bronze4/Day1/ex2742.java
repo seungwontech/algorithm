f53b885e65db7eb312054627f75f5c3862e75063
seungwon <swlee173@gmail.com>
2024-08-26 13:06:24 +0900

    브론즈4 - 기찍 N, 별 찍기2

---
+++ src/BaekJoon/Bronze4/Day1/ex2742.java
@@ -0,0 +1,13 @@
+package BaekJoon.Bronze4.Day1;
+
+import java.util.Scanner;
+
+public class ex2742 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = n; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
