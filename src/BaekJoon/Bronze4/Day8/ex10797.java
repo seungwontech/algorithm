36a165a44fb0d81d07e37d1ecc3cf6cfa27696c7
seungwon <swlee173@gmail.com>
2024-08-28 15:45:36 +0900

    브론즈4 - 인공지능 시계, 10부제

---
+++ src/BaekJoon/Bronze4/Day8/ex10797.java
@@ -0,0 +1,18 @@
+package BaekJoon.Bronze4.Day8;
+
+import java.util.Scanner;
+
+public class ex10797 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        for (int i = 0; i < 5; i++) {
+            int a = sc.nextInt();
+            if(a == n) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
