19500162ca9905f88c3723d43243d079595b07a5
seungwon <swlee173@gmail.com>
2024-08-28 15:44:36 +0900

    브론즈4 - 평균점수, R2

---
+++ src/BaekJoon/Bronze4/Day5/ex10039.java
@@ -0,0 +1,20 @@
+package BaekJoon.Bronze4.Day5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ex10039 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        for (int i =0 ; i < 5; i++){
+            int point = Integer.parseInt(br.readLine());
+            if(point < 40) {
+                point = 40;
+            }
+            sum += point;
+        }
+        System.out.println(sum/5);
+    }
+}
