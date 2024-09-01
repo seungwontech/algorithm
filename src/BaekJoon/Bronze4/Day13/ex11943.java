7e523df5373b92ee27a91605d752d80d338e3f39
seungwon <swlee173@gmail.com>
2024-09-01 16:20:50 +0900

    브론즈4 - 파일 옮기기, 특별한 날

---
+++ src/BaekJoon/Bronze4/Day13/ex11943.java
@@ -0,0 +1,16 @@
+package BaekJoon.Bronze4.Day13;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ex11943 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        System.out.println(Math.min(a +d, b +c));
+    }
+}
