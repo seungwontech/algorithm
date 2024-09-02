c7f9acc34431e2249389cb7692565f6175363ef1
seungwon <swlee173@gmail.com>
2024-09-02 14:06:22 +0900

    브론즈4 - 와글와글 숭고한, 줄번호

---
+++ src/BaekJoon/Bronze4/Day16/ex17388.java
@@ -0,0 +1,28 @@
+package BaekJoon.Bronze4.Day16;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ex17388 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int su = Integer.parseInt(st.nextToken());
+        int ku = Integer.parseInt(st.nextToken());
+        int hu = Integer.parseInt(st.nextToken());
+
+        if (su + ku + hu >= 100) {
+            System.out.println("OK");
+        } else {
+            if(su < ku && su < hu) {
+                System.out.println("Soongsil");
+            } else if(ku < su && ku < hu) {
+                System.out.println("Korea");
+            } else {
+                System.out.println("Hanyang");
+            }
+        }
+    }
+}
